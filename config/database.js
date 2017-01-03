1ef2d53829a99ae679a855d9465a9483287f3dc1
Colin Gidzinski <evg31337@gmail.com>
2017-01-02 21:05:29 -0500

    updated db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-	'secret': 'ilovescotcscscsctch',
-    'url' : 'mongodb://xendelta:xendelta@ds153765.mlab.com:53765/xendelta'
+	'secret': 'X30LU5v6k%06B7|$5FT83pteaT#Gv`',
+    'url' : 'mongodb://admacc:Q9495pU7E9Q7Z6zmUpfZOHK0X1U05WOA@ds151008.mlab.com:51008/phisoc'
 };
