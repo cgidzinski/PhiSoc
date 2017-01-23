040ad12be64be053eb7574dc2b48ae10ead718e1
Colin Gidzinski <evg31337@gmail.com>
2017-01-23 18:04:01 -0500

    Rem

--- config/database.js
+++
@@ -1,5 +0,0 @@
-// config/database.js
-module.exports = {
-	'secret': 'X30LU5v6k%06B7|$5FT83pteaT#Gv`',
-    'url' : 'mongodb://admacc:Q9495pU7E9Q7Z6zmUpfZOHK0X1U05WOA@ds151008.mlab.com:51008/phisoc'
-};
